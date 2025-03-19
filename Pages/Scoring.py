c5b2c4a38b741cfe32b0ee88d0b87648bbb02296
CarlosCO94 <164273378+CarlosCO94@users.noreply.github.com>
2025-03-19 17:17:14 -0500

    Delete Pages directory

--- Pages/Scoring.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-# Título de la página
-st.title("Scoring")
-st.write("Aquí puedes agregar la lógica para el scoring.")
--- Pages/Smart_11.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-# Título de la página
-st.title("Smart 11")
-st.write("Aquí puedes agregar la lógica para Smart 11.")
