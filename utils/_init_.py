56a23916e002ea015fa285436f4c1c60e0611cd0
CarlosCO94 <164273378+CarlosCO94@users.noreply.github.com>
2025-03-19 18:20:56 -0500

    Update _init_.py

--- utils/_init_.py
+++ utils/_init_.py
@@ -1,1 +1,8 @@
 
+# Inicialización del paquete utils
+# Este archivo es necesario para que Python reconozca la carpeta como un paquete
+
+# Importar funciones comunes para facilitar el acceso
+from utils.data_laloader import verificar_datos_cargados, obtener_datos
+from utils.data_processing import calcular_percentiles, encontrar_jugadores_similares
+from utils.visualization import grafico_radar_jugador, grafico_comparacion_barras
